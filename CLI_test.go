8192e7449349f1c720e101a408a7ca95efa28ee6
jw <choi.jae100@gmail.com>
2026-04-08 11:11:53 -0700

    Add unit tests for initial values

--- CLI_test.go
+++ CLI_test.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	trail "github.com/jwc20/oregontrail"
 )
 
@@ -14,7 +16,10 @@ func TestCLIInitSVT(t *testing.T) {
 	cli := trail.NewCLI(store, strings.NewReader(""), out)
 	cli.InitSVT()
 
-	if !cli.State.Trip.FortAvailable {
-		t.Error("FortAvailable should be true")
-	}
+	assert.True(t, cli.State.Trip.FortAvailable, "FortAvailable should be true")
+	assert.False(t, cli.State.Flags.Injured, "Injured should be false")
+	assert.False(t, cli.State.Flags.ClearedBlueMtns, "ClearedBlueMtns should be false")
+	assert.Equal(t, 2040, cli.State.Trip.Mileage, "Mileage should be 2040")
+	assert.False(t, cli.State.Flags.SouthPassMileage, "SouthPassMileage should be false")
+	assert.Equal(t, 0, cli.State.Trip.TurnNumber, "TurnNumber should be 0")
 }
