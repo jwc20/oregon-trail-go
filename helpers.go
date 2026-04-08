879cfc464f0aa250be1165199917fc405980236e
jw <choi.jae100@gmail.com>
2026-04-08 14:52:58 -0700

    Refactor GetRandomInt into smaller helpers

--- helpers.go
+++ helpers.go
@@ -9,33 +9,44 @@ import (
 	"time"
 )
 
-func newGetRandomInt() int {
-	url := "https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new"
+const url = "https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new"
 
-	client := &http.Client{
-		Timeout: time.Second * 10,
-	}
+func GetRandomInt() int {
+	request := newGetRandomIntRequest()
+	response := newGetRandomIntResponseFromClient(request)
+	defer response.Body.Close()
 
+	result := extractRandomInteger(response)
+	return result
+}
+
+func newGetRandomIntRequest() *http.Request {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return req
+}
 
+func newGetRandomIntResponseFromClient(req *http.Request) *http.Response {
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer resp.Body.Close()
+	return resp
+}
 
+func extractRandomInteger(resp *http.Response) int {
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	result, err := strconv.Atoi(strings.TrimSpace(string(body)))
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	return result
 }
--- helpers_test.go
+++ helpers_test.go
@@ -8,12 +8,10 @@ import (
 )
 
 func TestGetRandomIntRequest(t *testing.T) {
-
 	t.Run("returns a random number between 1 and 10", func(t *testing.T) {
-		result := newGetRandomInt()
+		result := GetRandomInt()
 
 		assert.GreaterOrEqual(t, result, 1, fmt.Sprintf("result %d should be >= 1", result))
 		assert.LessOrEqual(t, result, 10, fmt.Sprintf("result %d should be <= 10", result))
 	})
-
 }
