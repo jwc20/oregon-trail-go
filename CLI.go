7051989ec843d9ee4c642010a630c98a6fe9dc5f
jw <choi.jae100@gmail.com>
2026-04-08 11:25:56 -0700

    Add prompt for getting shooting level and unit test

--- CLI.go
+++ CLI.go
@@ -2,7 +2,10 @@ package oregontrail
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 type CLI struct {
@@ -32,9 +35,34 @@ func (cli *CLI) InitSVT() {
 	cli.State.Trip.TurnNumber = 0
 }
 
+func (cli *CLI) PromptShootingLevel() bool {
+	cli.printf("HOW GOOD A SHOT ARE YOU WITH YOUR RIFLE?\n")
+	cli.printf("  (1) ACE MARKSMAN\n")
+	cli.printf("  (2) GOOD SHOT\n")
+	cli.printf("  (3) FAIR TO MIDDLIN'\n")
+	cli.printf("  (4) NEED MORE PRACTICE\n")
+	cli.printf("  (5) SHAKY KNEES\n")
+	cli.printf("ENTER ONE OF THE ABOVE: ")
+
+	level, err := strconv.Atoi(strings.TrimSpace(cli.readLine()))
+	fmt.Printf("\n")
+	fmt.Fprint(cli.out, level)
+
+	if err != nil || level < 1 || level > 5 {
+		cli.printf("INVALID CHOICE\n")
+		return false
+	}
+	cli.State.Player.ShootingLevel = level
+	return true
+}
+
 // helper functions ***********************************
 
 func (cli *CLI) readLine() string {
 	cli.in.Scan()
 	return cli.in.Text()
+}
+
+func (cli *CLI) printf(format string, a ...interface{}) {
+	fmt.Fprintf(cli.out, format, a...)
 }
