5eea19970cc39a2c9507cfa6dc7fe971de2900ca
jw <choi.jae100@gmail.com>
2026-04-08 14:43:51 -0700

    Add newGetRandomInt using random.org

--- helpers.go
+++ helpers.go
@@ -1,1 +1,41 @@
 package oregontrail
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func newGetRandomInt() int {
+	url := "https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new"
+
+	client := &http.Client{
+		Timeout: time.Second * 10,
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := strconv.Atoi(strings.TrimSpace(string(body)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return result
+}
---
+++ helpers_test.go
@@ -0,0 +1,19 @@
+package oregontrail
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetRandomIntRequest(t *testing.T) {
+
+	t.Run("returns a random number between 1 and 10", func(t *testing.T) {
+		result := newGetRandomInt()
+
+		assert.GreaterOrEqual(t, result, 1, fmt.Sprintf("result %d should be >= 1", result))
+		assert.LessOrEqual(t, result, 10, fmt.Sprintf("result %d should be <= 10", result))
+	})
+
+}
