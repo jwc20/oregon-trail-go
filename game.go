e6d84f97df46657ac061ba307f6cc040c3c6a9d6
jw <choi.jae100@gmail.com>
2026-04-08 10:36:11 -0700

    Add structs for game variables

--- game.go
+++ game.go
@@ -1,1 +1,37 @@
 package oregontrail
+
+const TotalRequiredMileage = 2040
+
+type Player struct {
+	Cash          int
+	ShootingLevel int
+}
+
+type Inventory struct {
+	Oxen          int
+	Food          int
+	Ammo          int
+	Clothing      int
+	Miscellaneous int
+}
+
+type TripState struct {
+	Mileage       int
+	TurnNumber    int
+	FortAvailable bool
+}
+
+type Flags struct {
+	Injured          bool
+	Ill              bool
+	ClearedSouthPass bool
+	ClearedBlueMtns  bool
+	SouthPassMileage bool
+}
+
+type GameState struct {
+	Player    Player
+	Inventory Inventory
+	Trip      TripState
+	Flags     Flags
+}
